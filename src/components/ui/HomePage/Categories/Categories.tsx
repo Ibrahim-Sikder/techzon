e494bf9004f1f7688e4318875075f32fde015597
Ibrahim-Sikder <ibrahimsikder5033@gmail.com>
2024-03-25 19:22:49 +0600

    typescript-problem-solve

--- src/components/ui/HomePage/Categories/Categories.tsx
+++ src/components/ui/HomePage/Categories/Categories.tsx
@@ -7,16 +7,20 @@ import categories3 from "../../../../assets/images/categories6.png";
 import categories4 from "../../../../assets/images/categories7.png";
 import categories2 from "../../../../assets/images/categories.webp";
 import categories5 from "../../../../assets/images/categories3.png";
-import { HiMinus, HiOutlineEye, HiOutlineHeart, HiOutlineShoppingCart, HiStar } from "react-icons/hi";
+import {
+  HiMinus,
+  HiOutlineEye,
+  HiOutlineHeart,
+  HiOutlineShoppingCart,
+  HiStar,
+} from "react-icons/hi";
 import Image from "next/image";
-import ProductIcons from "../FlashSellProduct/ProductIcons";
-import CategoryIcons from "./CategoryIcons";
 import Link from "next/link";
 import TopRatedIcons from "../TopRatedProduct/TopRatedIcons";
 const Categories = () => {
   return (
     <Container className="sectionMargin">
-         <h3 className="text-3xl font-semibold mb-5">Our Popular Products </h3>
+      <h3 className="text-3xl font-semibold mb-5">Our Popular Products </h3>
       <div className="grid grid-cols-1 xl:grid-cols-12 lg:grid-cols-3  md:grid-cols-2 place-items-center gap-5 ">
         <div className="xl:col-span-2 col-span-1">
           <div className=" flashSellProductWrap categoriesCard  ">
@@ -24,7 +28,10 @@ const Categories = () => {
               <Image width="500" height="500" src={categories3} alt="flash" />
               <div>
                 <p className="flashCartName">Categories </p>
-               <Link href='/products'> <button className="flashCartBtn ">Add To Cart</button></Link>
+                <Link href="/products">
+                  {" "}
+                  <button className="flashCartBtn ">Add To Cart</button>
+                </Link>
                 <div className="flex items-center ">
                   <HiStar size={25} className=" startIcon" />
                   <HiStar size={25} className=" startIcon" />
@@ -39,15 +46,23 @@ const Categories = () => {
               </div>
             </div>
             <div className="iconWraps space-y-4">
-            <TopRatedIcons/>
-                <HiOutlineHeart className=" startIcon  startIcon2" size={30} />
-                <HiOutlineShoppingCart className=" startIcon startIcon2" size={30} />
-               
-              </div>
+              <TopRatedIcons />
+              <HiOutlineHeart className=" startIcon  startIcon2" size={30} />
+              <HiOutlineShoppingCart
+                className=" startIcon startIcon2"
+                size={30}
+              />
+            </div>
           </div>
           <div className=" flashSellProductWrap categoriesCard  ">
             <div className="flashContent">
-              <Image className="categories2Img" width="500" height="500" src={categories4} alt="flash" />
+              <Image
+                className="categories2Img"
+                width="500"
+                height="500"
+                src={categories4}
+                alt="flash"
+              />
               <div>
                 <p className="flashCartName">Categories </p>
                 <button className="flashCartBtn ">Add To Cart</button>
@@ -65,13 +80,13 @@ const Categories = () => {
               </div>
             </div>
             <div className="iconWraps space-y-4">
-            <TopRatedIcons/>
-                <HiOutlineHeart className=" startIcon  startIcon2" size={30} />
-                <HiOutlineShoppingCart className=" startIcon startIcon2" size={30} />
-               
-              </div>
-
-
+              <TopRatedIcons />
+              <HiOutlineHeart className=" startIcon  startIcon2" size={30} />
+              <HiOutlineShoppingCart
+                className=" startIcon startIcon2"
+                size={30}
+              />
+            </div>
           </div>
         </div>
         <div className=" flashSellProductWrap categoriesCard categoriesCard2  xl:col-span-8 middleCategories">
@@ -94,11 +109,13 @@ const Categories = () => {
             </div>
           </div>
           <div className="iconWraps space-y-4">
-          <TopRatedIcons/>
-                <HiOutlineHeart className=" startIcon  startIcon2" size={30} />
-                <HiOutlineShoppingCart className=" startIcon startIcon2" size={30} />
-               
-              </div>
+            <TopRatedIcons />
+            <HiOutlineHeart className=" startIcon  startIcon2" size={30} />
+            <HiOutlineShoppingCart
+              className=" startIcon startIcon2"
+              size={30}
+            />
+          </div>
         </div>
         <div className="xl:col-span-2 col-span-1 ">
           <div className=" flashSellProductWrap categoriesCard col-span-2 ">
@@ -120,7 +137,7 @@ const Categories = () => {
                 </div>
               </div>
               <div className="iconWraps space-y-4">
-              <TopRatedIcons/>
+                <TopRatedIcons />
                 <HiOutlineHeart className=" startIcon  startIcon2" size={30} />
                 <HiOutlineEye className=" startIcon startIcon2" size={30} />
               </div>
@@ -147,11 +164,13 @@ const Categories = () => {
               </div>
             </div>
             <div className="iconWraps space-y-4">
-            <TopRatedIcons/>
-                <HiOutlineHeart className=" startIcon  startIcon2" size={30} />
-                <HiOutlineShoppingCart className=" startIcon startIcon2" size={30} />
-               
-              </div>
+              <TopRatedIcons />
+              <HiOutlineHeart className=" startIcon  startIcon2" size={30} />
+              <HiOutlineShoppingCart
+                className=" startIcon startIcon2"
+                size={30}
+              />
+            </div>
           </div>
         </div>
       </div>
@@ -160,4 +179,3 @@ const Categories = () => {
 };
 
 export default Categories;
- 
