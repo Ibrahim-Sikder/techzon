10439b52b63760473e4f53a349c4704c1cf0acc0
Ibrahim-Sikder <ibrahimsikder5033@gmail.com>
2024-04-19 03:22:04 +0600

    add-product-modal-complete

--- src/app/(common)/dashboard/all-products/page.tsx
+++ src/app/(common)/dashboard/all-products/page.tsx
@@ -1,40 +1,76 @@
-import React from 'react';
-import './AllProduct.css'
-import { TProduct } from '@/types';
+'use client'
 
-const Page = async() => {
-  const res = await fetch('https://techzon-server.vercel.app/api/v1/products',{
-    next:{
-      revalidate: 30
-    }
-  })
+import React, { useState } from "react";
+import "./AllProduct.css";
+import { TProduct } from "@/types";
+import Image from "next/image";
+import { PiPlus } from "react-icons/pi";
+import EModal from "@/components/ui/EModal/EModal";
+
+const Page = async () => {
+  const [open, setOpen] = useState(false);
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
+
+
+  const res = await fetch("https://techzon-server.vercel.app/api/v1/products", {
+    next: {
+      revalidate: 30,
+    },
+  });
   const products = await res.json();
-
+ 
   return (
-    <div className="table-container">
-      <table className="attendanceInputTable">
-        <thead>
-          <tr>
-            <th>SL No</th>
-            <th>Name</th>
-            <th>Review</th>
-            <th>Categories</th>
-            <th>Discount</th>
-          </tr>
-        </thead>
-        <tbody>
-          {products?.data?.map((data: TProduct, i: number) => (
-            <tr className={i % 2 === 0 ? "even-row" : "odd-row"} key={data._id}>
-              <td>{i+1}</td>
-              <td>{data.name}</td>
-              <td>{data.review}</td>
-              <td>{data.categories}</td>
-              <td>{data.discount}</td>
+    <>
+    <div className="flex items-center justify-between mb-3">
+          <h3 className="text-3xl font-semibold ">All Products </h3>
+          <button onClick={handleOpen} className="addProductBtn flex items-center"><PiPlus size={20}/> <span className="ml-1 ">Add Products</span> </button>
+        </div>
+      <div className="table-container">
+        
+        <table className="attendanceInputTable">
+          <thead>
+            <tr>
+              <th>SL No</th>
+              <th>Items</th>
+              <th>Review</th>
+              <th>Categories</th>
+              <th>Discount</th>
             </tr>
-          ))}
-        </tbody>
-      </table>
-    </div>
+          </thead>
+          <tbody>
+            {products?.data?.map((data: TProduct, i: number) => (
+              <tr
+                className={i % 2 === 0 ? "even-row" : "odd-row"}
+                key={data._id}
+              >
+                <td>{i + 1}</td>
+                <td>
+                  <div className="flex items-center">
+                    <Image
+                      className="mr-3"
+                      alt="products"
+                      src={data.image}
+                      width="60"
+                      height="60"
+                    />
+                    <span>{data.name}</span>
+                  </div>
+                </td>
+                <td>{data.review}</td>
+                <td>{data.categories}</td>
+                <td>{data.discount}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+
+
+        {
+          open && <EModal onClose={handleClose}/>
+        }
+      </div>
+    </>
   );
 };
 
